5e74c7536bb3ecf6e852c5406cec0f087c4b5cd4
thijs oostdam <oostdam@gmail.com>
2020-03-05 19:14:59 +0100

    created tetrahedron scene. not sure if the fresnell calculation is correct

--- simple-scene/src/oostd/am/raytracer/scene/simple/SceneService.java
+++ simple-scene/src/oostd/am/raytracer/scene/simple/SceneService.java
@@ -1,11 +1,11 @@
 package oostd.am.raytracer.scene.simple;
 
 import oostd.am.raytracer.api.scenery.Scene;
-import oostd.am.raytracer.scene.simple.factory.Oktahedron;
+import oostd.am.raytracer.scene.simple.factory.TetraHedron;
 
 public class SceneService implements oostd.am.raytracer.api.scenery.SceneService {
     @Override
     public Scene getScene() {
-        return new Oktahedron().getScene();
+        return new TetraHedron().getScene();
     }
 }
